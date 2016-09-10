8b1c5911df0a32c211034d4e0eaf7fc851ffa671
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-09-10 02:06:49 +0200

    more work on A1 http

--- A1_rudy/src/http.erl
+++ A1_rudy/src/http.erl
@@ -18,8 +18,34 @@ parse_request(R0) ->
   {Body, _} = message_body(R2),
   {Request, Headers, Body}.
 
-request_line([$G, $E, $T, 32 |R0]) ->
+request_line([$G, $E, $T, 32 |R0]) -> % 32: space
   {URI, R1} = request_uri(R0),
   {Ver, R2} = http_version(R1),
-  [13,10|R3] = R2,
+  [13,10|R3] = R2,  % 13: carriage return, 10 line feed
   {{get, URI, Ver}, R3}.
+
+request_uri([32|R0]) ->
+  {[], R0};
+request_uri([C|R0]) ->
+  {Rest, R1} = request_uri(R0),
+  {[C|Rest], R1}.
+
+http_version([$H, $T, $T, $P, $/, $1, $., $1 | R0]) ->
+  {v11, R0};
+http_version([$H, $T, $T, $P, $/, $1, $., $0 | R0]) ->
+  {v10, R0}.
+
+headers([13,10|R0]) ->
+  {[],R0};
+headers(R0) ->
+  {Header, R1} = header(R0),
+  {Rest, R2} = headers(R1),
+  {[Header|Rest], R2}.
+header([13,10|R0]) ->
+  {[], R0};
+header([C|R0]) ->
+  {Rest, R1} = header(R0),
+  {[C|Rest], R1}.
+
+message_body(R) ->
+  {R, []}.
