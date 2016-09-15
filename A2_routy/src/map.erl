b7dc61265ef3ab4ed9cd6b9c4ba64361399aefb4
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-09-15 18:07:12 +0200

    route - map:update/3 done

--- A2_routy/src/map.erl
+++ A2_routy/src/map.erl
@@ -10,8 +10,58 @@
 -author("tts").
 
 %% API
--export([new/0]).
+-export([new/0, update/3, reachable/2, all_nodes/1, testUpdate/0]).
+
+% NOTES:
+% Directional map where you should easily be able to update the map and find nodes directly
+% connected to a given node.
+% List of entries where each entry consists of a city with a list of directly connected cities.
 
 % Returns an empty map (an empty list)
 new() ->
   [].
+
+% Update the map to reflect that Node has directional links to all nodes in the list Links.
+% The old entry is removed.
+update(Node, Links, Map) ->
+  % Remove old entry
+  % lists:keydelete(Key, N, TupleList1) -> TupleList2:
+  % Returns a copy of TupleList1 where the first occurrence of a tuple whose Nth element compares equal to Key
+  % is deleted, if there is such a tuple.
+  MapDeleted = lists:keydelete(Node, 1, Map),
+  % Append new entry
+  MapAppended = lists:append(MapDeleted, [{Node, Links}]),
+  MapAppended.
+
+% Returns the list of nodes directly reachable from Node.
+reachable(Node, Map) ->
+  ok.
+
+% Returns a list of all nodes in the map, also the ones without outgoing links.
+% So if berlin is is linked to london but london does not have any outgoing links (and thus no entry in the list),
+% london should still be in the returned list.
+all_nodes(Map) ->
+  ok.
+
+printMap([]) ->
+  io:format("[~p]map:printMap/1: []~n", [self()]),
+  ok;
+printMap(Map) ->
+  io:format("[~p]map:printMap/1: ~p~n", [self(), Map]),
+  ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TESTS
+
+testUpdate() ->
+  Map1 = map:update(berlin, [london, paris], []),
+  Map2 = map:update(reykjavik, [stockholm, berlin, helsinki], Map1),
+  Map3 = map:update(berlin, [this, works, now], Map2),
+  Success = listCompare(Map3, [{reykjavik,[stockholm,berlin,helsinki]},{berlin,[this,works,now]}]),
+  if
+    Success -> ok;
+    true -> error
+  end.
+
+listCompare(List, List) -> true;
+listCompare(_, _) -> false.
