c4650c7ead475f11af482c0c888a20d06a57ae93
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-09-27 15:49:28 +0200

    notes and comments

--- A3_loggy/src/worker.erl
+++ A3_loggy/src/worker.erl
@@ -18,6 +18,7 @@ start(Name, Logger, Seed, Sleep, Jitter) ->
 stop(Worker) ->
   Worker ! stop.
 
+% Initialize a worker. Note it will not start until it receives a peers message.
 init(Name, Log, Seed, Sleep, Jitter) ->
   random:seed(Seed, Seed, Seed),
   receive
@@ -27,6 +28,7 @@ init(Name, Log, Seed, Sleep, Jitter) ->
       ok
   end.
 
+% Start worker with peers
 peers(Wrk, Peers) ->
   Wrk ! {peers, Peers}.
 
@@ -34,6 +36,7 @@ loop(Name, Log, Peers, Sleep, Jitter) ->
   Wait = random:uniform(Sleep),
   receive
     {msg, Time, Msg} ->
+      % Received message from peer
       Log ! {log, Name, Time, {received, Msg}},
       loop(Name, Log, Peers, Sleep, Jitter);
     stop ->
@@ -41,18 +44,24 @@ loop(Name, Log, Peers, Sleep, Jitter) ->
     Error ->
       Log ! {log, Name, time, {error, Error}}
   after Wait ->
+    % Send message to random peer
     Selected = select(Peers),
+    % Dummy value for time, the worker does not know about the time
     Time = na,
+    % Create message with hopefully random number value
     Message = {hello, random:uniform(100)},
     Selected ! {msg, Time, Message},
+    % Delay between sending message to peer and sending a message to the logger
     jitter(Jitter),
     Log ! {log, Name, Time, {sending, Message}},
     loop(Name, Log, Peers, Sleep, Jitter)
   end.
 
+% Select a random peer from the list of peers
 select(Peers) ->
   lists:nth(random:uniform(length(Peers)), Peers).
 
+% Sleep for a random time
 jitter(0) ->
   ok;
 jitter(Jitter) ->
