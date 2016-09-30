674c0da882bfe4998105cbcdddaf8295b867d4b3
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-09-30 15:53:23 +0200

    gms1 add more given code

--- A4_groupy/src/gms1.erl
+++ A4_groupy/src/gms1.erl
@@ -12,6 +12,31 @@
 %% API
 -export([]).
 
+% Initialize a process that is the first node in a group
+% Give it an empty list of peers and let it know that its master is the only node in the group
+% (since it is the only node in the group it will of course be the leader of the group)
+start(Id) ->
+  Self = self(),
+  {ok, spawn_link(fun()-> init(Id, Self) end)}.
+init(Id, Master) ->
+  leader(Id, Master, [], [Master]).
+
+% Initialize a node that should join an existing group.
+start(Id, Grp) ->
+  Self = self(),
+  {ok, spawn_link(fun()-> init(Id, Grp, Self) end)}.
+init(Id, Grp, Master) ->
+  Self = self(),
+  % Send a join message to a node in the group and wait for an invitation
+  % Initial state of node will be a slave
+  Grp ! {join, Master, Self},
+  receive
+    {view, [Leader|Slaves], Group} ->
+      % Invitation received
+      Master ! {view, Group},
+      slave(Id, Master, Leader, Slaves, Group)
+  end.
+
 % Leader process
 % Id: a unique name, of the node, only used for debugging
 % Master: the process identifier of the application layer
@@ -34,6 +59,35 @@ leader(Id, Master, Slaves, Group) ->
       bcast(Id, {view, [self()|Slaves2], Group2}, Slaves2),
       Master ! {view, Group2},
       leader(Id, Master, Slaves2, Group2);
+    stop ->
+      ok
+  end.
+
+% Slave process
+% Id: a unique name, of the node, only used for debugging
+% Master: the process identifier of the application layer
+% Leader: the leader, must keep track of the leader
+% Slaves: an ordered list of the process identifiers of all slaves in the group (ordered by join time)
+% Group: a list of all application layer processes in the group
+slave(Id, Master, Leader, Slaves, Group) ->
+  receive
+    {mcast, Msg} ->
+      % a request from its master to multicast a message, the message is forwarded to the leader.
+      Leader ! {mcast, Msg},
+      slave(Id, Master, Leader, Slaves, Group);
+    {join, Wrk, Peer} ->
+      % a request from the master to allow a new node to join the group, the message is forwarded to the leader.
+      Leader ! {join, Wrk, Peer},
+      slave(Id, Master, Leader, Slaves, Group);
+    {msg, Msg} ->
+      % a multicasted message from the leader. A message Msg is sent to the master.
+      Master ! Msg,
+      slave(Id, Master, Leader, Slaves, Group);
+    {view, [Leader|Slaves2], Group2} ->
+      % {view, Peers, Group}
+      % a multicasted view from the leader. A view is delivered to the master process.
+      Master ! {view, Group2},
+      slave(Id, Master, Leader, Slaves2, Group2);
     stop ->
       ok
   end.
