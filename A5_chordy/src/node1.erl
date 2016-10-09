7d2264194905687439e3925f38f265f8ce693681
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-10-09 20:05:27 +0200

    (WIP) node1.erl

---
+++ A5_chordy/src/node1.erl
@@ -0,0 +1,70 @@
+%%%-------------------------------------------------------------------
+%%% @author tts
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 09. Oct 2016 17:42
+%%%-------------------------------------------------------------------
+-module(node1).
+-author("tts").
+
+%% API
+-export([node/3]).
+
+% First implementation that only handles a growing ring.
+
+% Predecessor and successor are of the form {Key, Pid}
+node(Id, Predecessor, Successor) ->
+  receive
+    {key, Qref, Peer} ->
+      % A peer needs to know our key
+      Peer ! {Qref, Id},
+      node(Id, Predecessor, Successor);
+    {notify, New} ->
+      % A new node informs us of its existence
+      Pred = notify(New, Id, Predecessor),
+      node(Id, Pred, Successor);
+    {request, Peer} ->
+      % A predecessor needs to know our predecessor
+      request(Peer, Predecessor),
+      node(Id, Predecessor, Successor);
+    {status, Pred} ->
+      % Our successor informs us about its predecessor
+      Succ = stabilize(Pred, Id, Successor),
+      node(Id, Predecessor, Succ);
+    stop ->
+      ok;
+    _ ->
+      io:format("Unknown message type")
+  end.
+
+% Pred: Our successor's current predecessor
+stabilize(Pred, Id, Successor) ->
+  {Skey, Spid} = Successor,
+  case Pred of
+    nil ->
+      % Inform our successor of our existence
+      something;
+    {Id, _} ->
+      % Pred points back to us, don't do anything
+      something;
+    {Skey, _} ->
+      % Pred is pointing back to itself (our successor's predecessor is our successor)
+      % Inform our successor of our existence
+      something;
+    {Xkey, Xpid} ->
+      % Pred is pointing to another node
+      % Should we slide ourselves between the two nodes or in front of the other node (successor's predecessor)
+      case key:between(Xkey, Id, Skey) of
+        true ->
+          % The other node is between us and our successor
+          % Adopt the other node as our successor and run stabilization again
+          something;
+        false ->
+          % The other node is not between us and our successor
+          % We should be in between our successor and and the other node
+          % Inform our successor of our existence
+          something
+      end
+  end.
